a3d5919b2a4e4a8d6e8cd64ed74607917a931fd9
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-02-22 00:04:20 +0100

    Initial dotfiles managment scripts and config

---
+++ default.nix
@@ -0,0 +1,167 @@
+{ pkgs ? import <nixpkgs> {}
+, repoUrl ? "https://github.com/dejanr/dotfiles.git"
+, channel ? "nixpkgs-unstable"
+, targetDir ? "$HOME/.dotfiles"
+}:
+
+let
+  install = pkgs.writeScript "install" ''
+    set -e
+
+    echo >&2
+    echo >&2 "Installing..."
+    echo >&2
+
+    nix-channel --add https://nixos.org/channels/${channel} nixpkgs
+    nix-channel --update nixpkgs
+
+    if [ ! -d ${targetDir} ]; then
+        echo "setting up dotfiles repository" >&2
+        mkdir -p ${targetDir}
+        git clone --depth=1 ${repoUrl} ${targetDir}
+    fi
+
+    ${link}
+  '';
+
+  link = pkgs.writeScript "link" ''
+    set -e
+
+    echo >&2
+    echo >&2 "Linking..."
+    echo >&2
+
+    mkdir -p ~/.config
+
+    for f in ${targetDir}/*; do
+      if [ -d $f  ]; then
+        echo "Relinking $f"
+        cd ${targetDir}
+        stow -t ~ -R $(basename $f)
+      fi
+    done
+  '';
+
+  unlink = pkgs.writeScript "unlink" ''
+    set -e
+
+    echo >&2
+    echo >&2 "Unlinking..."
+    echo >&2
+
+    for f in ${targetDir}/*; do
+      echo "Unlinking $f"
+      cd ${targetDir}
+      stow -t ~ -D $(basename $f)
+    done
+  '';
+
+  uninstall = pkgs.writeScript "uninstall" ''
+    ${unlink}
+
+    echo >&2
+    echo >&2 "Cleaning up..."
+    echo >&2
+
+    if [ -d ${targetDir} ]; then
+        echo "removing dotfiles repository" >&2
+        rm -rf ${targetDir}
+    fi
+  '';
+
+  switch = pkgs.writeScript "switch" ''
+    set -e
+
+    echo >&2
+    echo >&2 "Tagging working config..."
+    echo >&2
+
+    git branch -f update HEAD
+
+    echo >&2
+    echo >&2 "Switching environment..."
+    echo >&2
+
+    nixos-rebuild switch
+
+    ${link}
+
+    echo >&2
+    echo >&2 "Tagging updated..."
+    echo >&2
+
+    git branch -f working update
+    git branch -D update
+    git push
+  '';
+
+  update = pkgs.writeScript "update" ''
+    set -e
+
+    echo >&2
+    echo >&2 "Updating channels..."
+    echo >&2
+
+    nix-channel --update
+
+    ${switch}
+  '';
+in pkgs.stdenvNoCC.mkDerivation {
+  name = "dotfiles";
+  preferLocalBuild = true;
+  propagatedBuildInputs = [ pkgs.git pkgs.stow ];
+  propagatedUserEnvPkgs = [ pkgs.git pkgs.stow ];
+
+  unpackPhase = ":";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    echo "$shellHook" > $out/bin/dotfiles
+    chmod +x $out/bin/dotfiles
+  '';
+
+  shellHook = ''
+    set -e
+
+    while [ "$#" -gt 0 ]; do
+        i="$1"; shift 1
+        case "$i" in
+           help)
+                echo "dotfiles: [help] [install] [uninstall] [link] [unlink] [switch] [update]"
+                exit
+                ;;
+            link)
+                ${link}
+                ;;
+            switch)
+                ${switch}
+                ;;
+            unlink)
+                ${unlink}
+                ;;
+            uninstall)
+                ${uninstall}
+                ;;
+            update)
+                ${update}
+                ;;
+            *)
+                ${install}
+                ;;
+        esac
+    done
+    exit
+  '';
+
+  passthru.check = pkgs.stdenvNoCC.mkDerivation {
+     name = "run-dotfiles-test";
+     shellHook = ''
+        set -e
+        echo >&2 "running dotfiles tests..."
+        echo >&2
+        echo >&2 "checking repository"
+        test -d ${targetDir}
+        exit
+    '';
+  };
+}
