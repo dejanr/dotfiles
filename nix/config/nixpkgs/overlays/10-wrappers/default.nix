0d5c916eab2e999e259a7035b3154632cc55ebe5
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2019-03-21 22:36:43 +0100

    Add dunst wrapper

--- nix/config/nixpkgs/overlays/10-wrappers/default.nix
+++ nix/config/nixpkgs/overlays/10-wrappers/default.nix
@@ -5,11 +5,19 @@ self: super:
     inherit (self) colors fonts;
     inherit (super) stdenv makeWrapper writeTextFile alacritty;
   };
+
   termite = import ./termite {
     inherit (self) colors fonts;
     inherit (super) stdenv makeWrapper writeTextFile termite;
   };
+
   grobi = import ./grobi {
     inherit (super) stdenv makeWrapper writeTextFile grobi;
+  };
+
+  dunst = import ./dunst {
+    inherit (self) colors fonts;
+    inherit (super) stdenv makeWrapper writeTextFile dunst;
+    browser = "firefox";
   };
 }
