355af80ae2d781f8a81b4da3d513b9462ada6631
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-04-17 09:19:51 +0200

    Update grobi config for office, office-mirror and glass room

--- nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
+++ nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
@@ -13,13 +13,22 @@ rules:
     outputs_connected:
       - eDP1-AUO-9014-0
       - HDMI2-HPN-13411-16843009
-      - DP1-HWP-12910-16843009
     configure_row:
       - eDP1
-      - HDMI2
       - DP1
     execute_after:
-      - xrandr --output HDMI2 --pos 0x0 --output DP1 --pos 1920x0 --output eDP1 --pos 960x1200
+      - xrandr --output HDMI2 --mode 1920x1200 --scale 1.5x1.5 --pos 0x0 --output eDP1 --pos 0x1920
+      - wm-wallpaper
+
+  - name: office-mirror
+    outputs_connected:
+      - eDP1-AUO-9014-0
+      - HDMI2-HPN-13411-16843009
+    configure_row:
+      - eDP1
+      - DP1
+    execute_after:
+      - xrandr --output HDMI2 --auto --scale-from 2560x1440 --output eDP1
       - wm-wallpaper
 
   - name: office-team-glass-room
@@ -34,6 +43,21 @@ rules:
       - eDP1
       - DP1
     execute_after:
+      - wm-wallpaper
+
+  - name: office-team-glass-room-mirror
+    outputs_connected:
+      - eDP1-AUO-9014-0
+      - DP1-ACR-1040-1361092661
+
+    configure_row:
+      - eDP1
+      - DP1
+    configure_row:
+      - eDP1
+      - DP1
+    execute_after:
+      - xrandr --output DP1 --auto --scale-from 2560x1440 --output eDP1
       - wm-wallpaper
 
   - name: mobile
