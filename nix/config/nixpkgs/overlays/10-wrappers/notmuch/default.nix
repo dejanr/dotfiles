c57cc3b3f4c9ad2d013516fc8e73bb6da6bd50a7
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-05-25 09:14:39 +0200

    Wrap and customize notmuch mail indexing and searching

---
+++ nix/config/nixpkgs/overlays/10-wrappers/notmuch/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, makeWrapper, writeTextFile, notmuch }:
+
+let
+  config = import ./config.nix { };
+  configFile = writeTextFile {
+    name = "config";
+    text = config;
+  };
+in stdenv.mkDerivation {
+  name = "notmuch-wrapper";
+  buildInputs = [ makeWrapper ];
+  phases = [ "buildPhase" ];
+  buildCommand = ''
+    mkdir -p $out/bin
+    mkdir -p $out/include
+    mkdir -p $out/lib
+    mkdir -p $out/share
+    makeWrapper ${notmuch}/bin/notmuch $out/bin/notmuch --add-flags "--config ${configFile}"
+    makeWrapper ${notmuch}/bin/notmuch-emacs-mua $out/bin/notmuch-emacs-mua --add-flags "--config ${configFile}"
+    cp -R ${notmuch}/include $out/
+    cp -R ${notmuch}/lib $out/
+    cp -R ${notmuch}/share $out/
+  '';
+}
