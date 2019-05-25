1caa196e9fab65178ee84fca636c2fd27fa72c3b
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-05-25 09:13:45 +0200

    Add isync for syncing mailboxes

---
+++ nix/config/nixpkgs/overlays/10-wrappers/isync/default.nix
@@ -0,0 +1,18 @@
+{ stdenv, makeWrapper, writeTextFile, isync }:
+
+let
+  config = import ./config.nix { };
+  configFile = writeTextFile {
+    name = "config";
+    text = config;
+  };
+in stdenv.mkDerivation {
+  name = "isync-wrapper";
+  buildInputs = [ makeWrapper ];
+  propagatedBuildInputs = [ isync ];
+  phases = [ "buildPhase" ];
+  buildCommand = ''
+    mkdir -p $out/bin
+    makeWrapper ${isync}/bin/mbsync $out/bin/mbsync --add-flags "-c ${configFile}"
+  '';
+}
