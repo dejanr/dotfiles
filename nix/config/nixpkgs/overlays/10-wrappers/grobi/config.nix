df121e9e857d282aa4408b4b8ace18ff156b47a1
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-10-18 15:15:44 +0200

    Update grobi config

--- nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
+++ nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
@@ -12,22 +12,16 @@ rules:
   - name: office
     outputs_connected:
       - HDMI2-GSM-30484-304405-LG
-    configure_row:
-      - DP1
+    configure_single: HDMI2
     execute_after:
-      - xrandr --dpi 123 --output eDP1 --off --output HDMI2 --mode 2560x1080 --rate 60 --pos 0x0 --primary
-      - echo "Xft.dpi: 123" | xrdb -merge
+      - xrandr --output eDP1 --off --output HDMI2 --mode 2560x1080 --rate 60 --pos 0x0 --primary
       - wm-wallpaper
 
-  - name: office-mirror
-    outputs_connected:
-      - eDP1-AUO-9014-0
-      - HDMI2-HPN-13411-16843009
-    configure_row:
-      - eDP1
-      - DP1
+  - name: mobile
+    outputs_disconnected:
+      - HDMI2
+    configure_single: eDP1
     execute_after:
-      - xrandr --output HDMI2 --auto --scale-from 2560x1440 --output eDP1
       - wm-wallpaper
 
   - name: office-team-glass-room
@@ -57,13 +51,6 @@ rules:
       - DP1
     execute_after:
       - xrandr --output DP1 --auto --scale-from 2560x1440 --output eDP1
-      - wm-wallpaper
-
-  - name: mobile
-    outputs_disconnected:
-      - HDMI2
-    configure_single: eDP1
-    execute_after:
       - wm-wallpaper
 
   - name: Fallback
