e7c9092a316aab937972d541f8abaecbd1fad9a3
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-11-12 18:51:34 +0100

    Home machine

---
+++ nix/config/nixpkgs/machines/home/configuration.nix
@@ -0,0 +1,106 @@
+{ config, lib, pkgs, ... }:
+
+let
+  hostName = "home";
+  fancontrol = import ./fancontrol.nix {};
+in {
+  imports =
+    [
+    ./hardware-configuration.nix
+    ../../roles/common.nix
+    ../../roles/fonts.nix
+    ../../roles/multimedia.nix
+    ../../roles/desktop.nix
+    ../../roles/i3.nix
+    ../../roles/development.nix
+    ../../roles/services.nix
+    ../../roles/email-client.nix
+   ];
+
+  nix.useSandbox = false;
+
+  networking = {
+    hostName = hostName;
+    hostId = "8425e349";
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+      useGlamor = true;
+      modules = [ pkgs.xf86_input_mtrack ];
+      videoDrivers = [ "amdgpu" "mesa" "modesetting" ];
+
+      synaptics.enable = false;
+
+      libinput = {
+        enable = true;
+        disableWhileTyping = true;
+        scrollMethod = "twofinger";
+        tapping = true;
+      };
+
+      deviceSection = ''
+        Option "TearFree" "true"
+        Option "DRI" "3"
+        Option "VariableRefresh" "true"
+      '';
+    };
+
+    tlp = {
+      enable = true;
+      extraConfig = ''
+        CPU_SCALING_GOVERNOR_ON_AC=performance
+        CPU_SCALING_GOVERNOR_ON_BAT=ondemand
+        SCHED_POWERSAVE_ON_AC=0
+        SCHED_POWERSAVE_ON_BAT=1
+        ENERGY_PERF_POLICY_ON_AC=performance
+        ENERGY_PERF_POLICY_ON_BAT=powersave
+        PCIE_ASPM_ON_AC=performance
+        PCIE_ASPM_ON_BAT=powersave
+        WIFI_PWR_ON_AC=1
+        WIFI_PWR_ON_BAT=5
+        RUNTIME_PM_ON_AC=on
+        RUNTIME_PM_ON_BAT=auto
+        USB_BLACKLIST_WWAN=1
+        USB_AUTOSUSPEND=0
+        CONTROL_USB_AUTOSUSPEND="off"
+        DEVICES_TO_DISABLE_ON_STARTUP=""
+      '';
+    };
+  };
+
+  environment = {
+    etc."X11/Xresources".text = ''
+      Xft.dpi: 109
+    '';
+  };
+
+  programs.light.enable = true;
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.storageDriver = "zfs";
+
+  systemd.services.fancontrol = {
+    description = "Start fancontrol";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.lm_sensors}/sbin/fancontrol";
+    };
+  };
+
+  systemd.services.fancontrolRestart = {
+    description = "Restart fancontrol on resume";
+    wantedBy = [ "suspend.target" ];
+    after = [ "suspend.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.systemd}/bin/systemctl --no-block restart fancontrol";
+    };
+  };
+
+  environment.etc."fancontrol".text = fancontrol;
+
+  system.stateVersion = "19.09";
+}
---
+++ nix/config/nixpkgs/machines/home/hardware-configuration.nix
@@ -0,0 +1,89 @@
+{ config, lib, pkgs, ... }:
+
+# ASRock A300
+# Ryzen 2400G
+
+{
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
+
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "lm92" ];
+    initrd.supportedFilesystems = [ "zfs" ];
+    kernelModules = [ "kvm-amd" "nct6775" "k10temp" "coretemp" ];
+    kernelPackages = pkgs.linuxPackages_5_2;
+    kernelParams = [
+      "quiet" "loglevel=3" "vga=current" # quiet boot
+    ];
+    blacklistedKernelModules = [
+      "sp5100-tco"
+    ];
+
+    extraModprobeConfig = ''
+      options k10temp force=1
+    '';
+
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "1048576";
+      "kernel.nmi_watchdog" = 0;
+    };
+
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    supportedFilesystems = [ "zfs" "exfat" ];
+    zfs.enableUnstable = true;
+    cleanTmpDir = true;
+  };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+
+    cpu = {
+      amd.updateMicrocode = true;
+    };
+
+    opengl = {
+      enable =true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-media-driver # only available starting nixos-19.03 or the current nixos-unstable
+      ];
+    };
+
+    firmware = [
+      pkgs.firmwareLinuxNonfree
+    ];
+
+    enableRedistributableFirmware = true;
+  };
+
+  fileSystems."/" =
+    { device = "zpool/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    { device = "zpool/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/4182-AF1E";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  nix.maxJobs = lib.mkDefault 8;
+  i18n.consoleFont = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+}
