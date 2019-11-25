17fa8d7b24cdd4edb8b7b4136ff3143e01518e79
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-11-25 09:20:37 +0100

    Update fan control for home pc

--- nix/config/nixpkgs/machines/home/fancontrol.nix
+++ nix/config/nixpkgs/machines/home/fancontrol.nix
@@ -6,9 +6,9 @@ DEVPATH=hwmon0=devices/platform/nct6775.656 hwmon1=devices/pci0000:00/0000:00:18
 DEVNAME=hwmon0=nct6793 hwmon1=k10temp
 FCTEMPS=hwmon0/pwm2=hwmon1/temp1_input
 FCFANS= hwmon0/pwm2=hwmon0/fan2_input
-MINTEMP=hwmon0/pwm2=50
-MINPWM=50
-MINSTART=hwmon0/pwm2=80
+MINTEMP=hwmon0/pwm2=45
+MINPWM=100
+MINSTART=hwmon0/pwm2=100
 MINSTOP=hwmon0/pwm2=50
 MAXTEMP=hwmon0/pwm2=80
 MAXPWM=240
