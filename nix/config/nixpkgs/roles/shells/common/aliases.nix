6f87cc315221665058870d06efd141d63a1d98d6
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2020-07-05 09:01:08 +0200

    Add vnc aliases

--- nix/config/nixpkgs/roles/shells/common/aliases.nix
+++ nix/config/nixpkgs/roles/shells/common/aliases.nix
@@ -91,6 +91,9 @@ in
     gpom = "git push origin HEAD:refs/for/master";
     gitv = "vim -c 'Gitv' .";
 
+    vnc-server = "x11vnc -repeat -forever -noxrecord -noxdamage -rfbport 5900";
+    vnc = "vncviewer −FullscreenSystemKeys -MenuKey F1";
+
     # wget as regular browser
     wgets = "wget --referer='http://www.google.com' --user-agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6' --header='Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5' --header='Accept-Language: en-us,en;q=0.5' --header='Accept-Encoding: gzip,deflate' --header='Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7' --header='Keep-Alive: 300'";
 
