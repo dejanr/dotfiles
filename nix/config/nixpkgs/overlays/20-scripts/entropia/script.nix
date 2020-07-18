d455256806bb3126c65b600fd2ceed2f01295eac
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2020-07-19 01:16:20 +0200

    Update eu scripts

--- nix/config/nixpkgs/overlays/20-scripts/entropia/script.nix
+++ nix/config/nixpkgs/overlays/20-scripts/entropia/script.nix
@@ -1,5 +1,5 @@
 { wine, prefix }: ''
   #!/usr/bin/env sh
 
-  cd ~/${prefix}/drive_c/Program\ Files\ \(x86\)/Entropia\ Universe && WINEPREFIX=~/${prefix} ${wine} bin32/ClientLoader.exe 1>/dev/null 2>/dev/null
+  cd ~/${prefix}/drive_c/Program\ Files\ \(x86\)/Entropia\ Universe && WINEDEBUG=-all WINEPREFIX=~/${prefix} ${wine} bin32/ClientLoader.exe 1>/dev/null 2>/dev/null
 ''
