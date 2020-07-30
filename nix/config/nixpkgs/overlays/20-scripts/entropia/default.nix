872e6d01a6507086a2be4bb7d746791b6abf9bc0
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2020-07-30 12:08:11 +0200

    Add euxs script

--- nix/config/nixpkgs/overlays/20-scripts/entropia/default.nix
+++ nix/config/nixpkgs/overlays/20-scripts/entropia/default.nix
@@ -11,16 +11,22 @@ let
     wine = pkgs.wine;
     prefix = ".entropia-fs";
   };
+  euxs = import ./script.nix {
+    wine = pkgs.wine;
+    prefix = ".entropia-xs";
+  };
 in
 stdenv.mkDerivation {
   name = name;
   euScript = writeScript name eu;
   eufsScript = writeScript name eufs;
+  euxsScript = writeScript name euxs;
   phases = [ "installPhase" ];
   installPhase = ''
     mkdir -p $out/bin
     echo "$euScript" > $out/bin/entropia
     echo "$eufsScript" > $out/bin/entropia-fs
+    echo "$euxsScript" > $out/bin/entropia-xs
     chmod +x $out/bin/entropia
     chmod +x $out/bin/entropia-fs
   '';
