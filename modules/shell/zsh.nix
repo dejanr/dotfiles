8eb3342ad8245ba1824be0702c7fe3e875f80e04
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2020-08-06 11:16:23 +0200

    Add nix shell zsh

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,8 +1,7 @@
 # modules/shell/zsh.nix --- ...
 
 { config, options, pkgs, lib, ... }:
-with lib;
-{
+with lib; {
   options.modules.shell.zsh = {
     enable = mkOption {
       type = types.bool;
@@ -24,13 +23,13 @@ with lib;
         tldr
         tree
       ];
-      env.ZDOTDIR   = "$XDG_CONFIG_HOME/zsh";
+      env.ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
       env.ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
 
       alias.exa = "exa --group-directories-first";
-      alias.l   = "exa -1";
-      alias.ll  = "exa -lg";
-      alias.la  = "LC_COLLATE=C exa -la";
+      alias.l = "exa -1";
+      alias.ll = "exa -lg";
+      alias.la = "LC_COLLATE=C exa -la";
       alias.sc = "systemctl";
       alias.ssc = "sudo systemctl";
 
@@ -39,16 +38,20 @@ with lib;
         source = <config/zsh>;
         recursive = true;
       };
-    };
 
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      # I init completion myself, because enableGlobalCompInit initializes it too
-      # soon, which means commands initialized later in my config won't get
-      # completion, and running compinit twice is slow.
-      enableGlobalCompInit = false;
-      promptInit = "";
+      home.programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        plugins = [{
+          name = "zsh-nix-shell";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.1.0";
+            sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+          };
+        }];
+      };
     };
 
     system.userActivationScripts.cleanupZgen = "rm -fv $XDG_CACHE_HOME/zsh/*";
