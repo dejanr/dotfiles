8c1f8344bc6cfb72fefd0dadc5429b43c94007e7
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2021-04-23 13:24:38 +0200

    Bump nodejs to 15

--- nix/config/nixpkgs/roles/common.nix
+++ nix/config/nixpkgs/roles/common.nix
@@ -65,6 +65,8 @@ in
     t
     wm-lock
     wm-wallpaper
+    music
+    youtube-dl
 
     # nixpkgs
     apg # Tools for random password generation
@@ -82,7 +84,7 @@ in
     fd # A simple, fast and user-friendly alternative to find
     linuxPackages.cpupower # Tool to examine and tune power saving features
     niv # dependency manager for nix projects
-    nodejs
+    nodejs-15_x
     neovim
     pixz # A parallel compressor/decompressor for xz format
     psmisc # A set of small useful utilities that use the proc filesystem (such as fuser, killall and pstree)
