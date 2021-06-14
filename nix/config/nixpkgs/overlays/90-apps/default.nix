29648a082e60dc9aefccce1d43fe816974164b73
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2021-06-14 11:00:49 +0200

    Update dxvk

--- nix/config/nixpkgs/overlays/90-apps/default.nix
+++ nix/config/nixpkgs/overlays/90-apps/default.nix
@@ -2,15 +2,27 @@ self: super:
 let
   emacsWithPackages = (super.emacsPackagesNgGen super.emacsGit).emacsWithPackages;
   epkgs = super.epkgs.melpaStablePackages;
+
+  nativeStdenv = super.impureUseNativeOptimizations super.stdenv;
+
+  withFlags = pkg: flags:
+    pkg.overrideAttrs (old: {
+      NIX_CFLAGS_COMPILE = old.NIX_CFLAGS_COMPILE or "" +
+      super.lib.concatMapStrings (x: " " + x) flags;
+    });
+
+  withStdenv = newStdenv: pkg:
+    pkg.override { stdenv = newStdenv; };
+
+  withStdenvAndFlags = newStdenv: pkg:
+    withFlags (withStdenv newStdenv pkg);
+
+  withNativeAndFlags = withStdenvAndFlags nativeStdenv;
 in
 {
   pragmatapro = super.callPackage ./pragmatapro/default.nix {};
 
   dxvk = super.callPackage ./dxvk {};
-
-  wine = super.callPackage ./wine {
-    inherit (super) fetchFromGitHub wine git perl utillinux autoconf python3;
-  };
 
   pyfa = super.callPackage ./pyfa {
     inherit (super) python3 fetchurl makeDesktopItem writeScriptBin;
--- nix/config/nixpkgs/overlays/90-apps/dxvk/default.nix
+++ nix/config/nixpkgs/overlays/90-apps/dxvk/default.nix
@@ -1,25 +1,22 @@
- { lib, stdenv, fetchurl }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   pname = "dxvk";
-  version = "1.8";
+  version = "1.8.1";
 
   src = fetchurl {
     url = "https://github.com/doitsujin/dxvk/releases/download/v${version}/dxvk-${version}.tar.gz";
-    sha256 = "6E96xJSsf1ATl2dERwiZIm0UXilhfEB/9ShwBVvaR24=";
+    sha256 = "dWoJxG+Cea3oRFbjrwOPZGE6UeAKLUz/+kyRwQ7eYOg=";
   };
 
   phases = "unpackPhase installPhase fixupPhase";
 
   installPhase = ''
     mkdir -p $out/share/dxvk/
-
     cp setup_dxvk.sh $out/share/dxvk/setup_dxvk
     chmod +x $out/share/dxvk/setup_dxvk
-
     mkdir -p $out/bin/
     ln -s $out/share/dxvk/setup_dxvk $out/bin/setup_dxvk
-
     cp -r x64/ $out/share/dxvk/
     cp -r x32/ $out/share/dxvk/
   '';
@@ -28,7 +25,7 @@ stdenv.mkDerivation rec {
     patchShebangs $out/share/dxvk/setup_dxvk
   '';
 
-  meta = with lib; {
+  meta = with stdenv.lib; {
     platforms = platforms.linux;
     licenses = [ licenses.zlib licenses.png ];
   };
