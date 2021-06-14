71e2109e441156de65be2debf88a57c7bd62712d
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2021-06-14 11:03:07 +0200

    Move to kernel 5.11, add patch for disk_cache, use latest mesa

--- nix/config/nixpkgs/machines/omega/configuration.nix
+++ nix/config/nixpkgs/machines/omega/configuration.nix
@@ -5,70 +5,70 @@ let
   username = "dejanr";
   hostName = "omega";
 in
-{
-  imports = [
-    ./hardware-configuration.nix
-    ../../roles/common.nix
-    ../../roles/hosts.nix
-    ../../roles/shells/bash
-    ../../roles/fonts.nix
-    ../../roles/desktop.nix
-    ../../roles/multimedia.nix
-    ../../roles/i3.nix
-    ../../roles/services.nix
-    ../../roles/development.nix
-    ../../roles/games.nix
-    ../../roles/games/valheim.nix
-  ];
+  {
+    imports = [
+      ./hardware-configuration.nix
+      ../../roles/common.nix
+      ../../roles/hosts.nix
+      ../../roles/shells/bash
+      ../../roles/fonts.nix
+      ../../roles/desktop.nix
+      ../../roles/multimedia.nix
+      ../../roles/i3.nix
+      ../../roles/services.nix
+      ../../roles/development.nix
+      ../../roles/games.nix
+      ../../roles/games/valheim.nix
+    ];
 
-  networking = {
-    hostId = "8425e349";
-    hostName = "${hostName}";
-  };
+    networking = {
+      hostId = "8425e349";
+      hostName = "${hostName}";
+    };
 
-  services = {
-    xserver = {
-      enable = true;
-      useGlamor = true;
-      videoDrivers = [ "amdgpu" ];
+    services = {
+      xserver = {
+        enable = true;
+        useGlamor = true;
+        videoDrivers = [ "amdgpu" ];
 
-      displayManager = {
-        xserverArgs = [ "-dpi 109" ];
+        displayManager = {
+          xserverArgs = [ "-dpi 109" ];
+        };
+      };
+
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
+          SCHED_POWERSAVE_ON_AC = 0;
+          SCHED_POWERSAVE_ON_BAT = 1;
+          ENERGY_PERF_POLICY_ON_AC = "performance";
+          ENERGY_PERF_POLICY_ON_BAT = "powersave";
+          PCIE_ASPM_ON_AC = "performance";
+          PCIE_ASPM_ON_BAT = "powersave";
+          WIFI_PWR_ON_AC = 1;
+          WIFI_PWR_ON_BAT = 5;
+          RUNTIME_PM_ON_AC = "on";
+          RUNTIME_PM_ON_BAT = "auto";
+          USB_BLACKLIST_WWAN = 1;
+          SOUND_POWER_SAVE_ON_BAT = 0;
+          USB_AUTOSUSPEND = 0;
+          CONTROL_USB_AUTOSUSPEND = "off";
+          DEVICES_TO_DISABLE_ON_STARTUP = "";
+        };
       };
     };
 
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
-        SCHED_POWERSAVE_ON_AC = 0;
-        SCHED_POWERSAVE_ON_BAT = 1;
-        ENERGY_PERF_POLICY_ON_AC = "performance";
-        ENERGY_PERF_POLICY_ON_BAT = "powersave";
-        PCIE_ASPM_ON_AC = "performance";
-        PCIE_ASPM_ON_BAT = "powersave";
-        WIFI_PWR_ON_AC = 1;
-        WIFI_PWR_ON_BAT = 5;
-        RUNTIME_PM_ON_AC = "on";
-        RUNTIME_PM_ON_BAT = "auto";
-        USB_BLACKLIST_WWAN = 1;
-        SOUND_POWER_SAVE_ON_BAT = 0;
-        USB_AUTOSUSPEND = 0;
-        CONTROL_USB_AUTOSUSPEND = "off";
-        DEVICES_TO_DISABLE_ON_STARTUP = "";
-      };
+    environment = {
+      etc."X11/Xresources".text = ''
+        Xft.dpi: 109
+      '';
+      systemPackages = [ ];
     };
-  };
 
-  environment = {
-    etc."X11/Xresources".text = ''
-      Xft.dpi: 109
-    '';
-    systemPackages = [ ];
-  };
+    virtualisation.docker.enableNvidia = true;
 
-  virtualisation.docker.enableNvidia = true;
-
-  system.stateVersion = "20.03";
-}
+    system.stateVersion = "20.09";
+  }
--- nix/config/nixpkgs/machines/omega/hardware-configuration.nix
+++ nix/config/nixpkgs/machines/omega/hardware-configuration.nix
@@ -17,10 +17,10 @@
     ];
 
     extraModulePackages = with pkgs; [
-      linuxPackages_latest.v4l2loopback
+      linuxPackages_5_11.v4l2loopback
     ];
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_5_11;
 
     kernel.sysctl = {
       "fs.inotify.max_user_watches" = 524288;
@@ -43,7 +43,24 @@
           sha256 = "2hHSMHtr4B0bZ1zehOJL1NMgVFgOT+gS+TDb3IgS3x4=";
         };
       };
-    in [ fsync ];
+
+      futex2 = rec {
+        name = "v5.11-futex2";
+        patch = pkgs.fetchpatch {
+          name = name + ".patch";
+          url = "https://raw.githubusercontent.com/sirlucjan/kernel-patches/master/5.11/futex2-dev-trunk-patches-v4/0001-futex2-resync-from-gitlab.collabora.com.patch";
+          sha256 = "a/5TL1OLTC7WILIKA1Vprwdgp2mo7tf3VCukyACdvcI=";
+        };
+      };
+
+      enableFutex2 = {
+        name = "futex2-config";
+        patch = null;
+        extraConfig = ''
+          FUTEX2 y
+        '';
+      };
+    in [ fsync futex2 enableFutex2 ];
 
     blacklistedKernelModules = [
       "fbcon"
@@ -55,7 +72,6 @@
       options k10temp force=1
       options v4l2loopback exclusive_caps=1 video_nr=9 card_label=v4l2
     '';
-
 
     supportedFilesystems = [ "btrfs" ];
 
@@ -82,18 +98,18 @@
 
   fileSystems."/" =
     { device = "/dev/nvme0n1p3";
-      fsType = "btrfs";
-      options = [ "subvol=nixos" ];
-    };
+    fsType = "btrfs";
+    options = [ "subvol=nixos" ];
+  };
 
   fileSystems."/boot" =
     { device = "/dev/nvme0n1p1";
-      fsType = "vfat";
-    };
+    fsType = "vfat";
+  };
 
   swapDevices =
     [ { device = "/dev/nvme0n1p2"; }
-    ];
+  ];
 
   hardware = {
     cpu = {
@@ -102,23 +118,60 @@
 
     video.hidpi.enable = lib.mkDefault true;
 
-    opengl = {
-      enable = true;
+    opengl = let
+      mesaDrivers = pkgs: ((pkgs.mesa.override {
+        stdenv = pkgs.impureUseNativeOptimizations (if !pkgs.stdenv.is32bit then
+        pkgs.llvmPackages_latest.stdenv
+        else
+        pkgs.stdenv);
+
+        galliumDrivers = [ "radeonsi" "virgl" "svga" "swrast" "zink" ];
+      }).overrideAttrs (oldAttrs: rec {
+        version = "21.0.0";
+
+        src = pkgs.fetchgit {
+          url = "https://gitlab.freedesktop.org/mesa/mesa.git";
+          # 01-30-21
+          rev = "205e737f51baf2958c047ae6ce3af66bffb52b37";
+          sha256 = "WkGiW06wEnDHTr2dIVHAcZlWLMvacHh/m4P+eVD4huI=";
+        };
+
+        mesonFlags = oldAttrs.mesonFlags ++ [
+          "-Dmicrosoft-clc=disabled"
+          "-Dosmesa=true"
+        ];
+
+        # For zink driver
+        buildInputs = oldAttrs.buildInputs ++ [
+          pkgs.vulkan-loader
+        ];
+
+        patches = [
+          ./patches/disk_cache-include-dri-driver-path-in-cache-key.patch
+        ];
+      })).drivers;
+    in {
       driSupport = true;
       driSupport32Bit = true;
-      extraPackages = [
-        pkgs.rocm-opencl-icd
-        pkgs.rocm-opencl-runtime
-        pkgs.amdvlk
+
+      package = mesaDrivers pkgs;
+      package32 = mesaDrivers pkgs.pkgsi686Linux;
+
+      extraPackages = with pkgs; [
+        amdvlk
+        rocm-opencl-icd
+        rocm-opencl-runtime
       ];
-      extraPackages32 = [
-        pkgs.driversi686Linux.amdvlk
+
+      extraPackages32 = with pkgs; [
+        driversi686Linux.amdvlk
       ];
     };
 
     firmware = [
       pkgs.firmwareLinuxNonfree
     ];
+
     enableRedistributableFirmware = true;
   };
 
