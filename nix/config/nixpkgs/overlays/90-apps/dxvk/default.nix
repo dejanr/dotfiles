96c8f8a492cafe5454c9522f0b784a4b1b0a9dd1
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2021-07-28 02:20:39 +0200

    update dxvk

--- nix/config/nixpkgs/overlays/90-apps/dxvk/default.nix
+++ nix/config/nixpkgs/overlays/90-apps/dxvk/default.nix
@@ -1,22 +1,25 @@
-{ stdenv, fetchurl }:
+{ stdenv, lib, fetchurl }:
 
 stdenv.mkDerivation rec {
   pname = "dxvk";
-  version = "1.8.1";
+  version = "1.9.1";
 
   src = fetchurl {
     url = "https://github.com/doitsujin/dxvk/releases/download/v${version}/dxvk-${version}.tar.gz";
-    sha256 = "dWoJxG+Cea3oRFbjrwOPZGE6UeAKLUz/+kyRwQ7eYOg=";
+    sha256 = "73WR1u/8yoqDUs6k+lD+c6ofEP2Jy0dfLxQjbkNAoAc=";
   };
 
   phases = "unpackPhase installPhase fixupPhase";
 
   installPhase = ''
     mkdir -p $out/share/dxvk/
+
     cp setup_dxvk.sh $out/share/dxvk/setup_dxvk
     chmod +x $out/share/dxvk/setup_dxvk
+
     mkdir -p $out/bin/
     ln -s $out/share/dxvk/setup_dxvk $out/bin/setup_dxvk
+
     cp -r x64/ $out/share/dxvk/
     cp -r x32/ $out/share/dxvk/
   '';
@@ -25,7 +28,7 @@ stdenv.mkDerivation rec {
     patchShebangs $out/share/dxvk/setup_dxvk
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     platforms = platforms.linux;
     licenses = [ licenses.zlib licenses.png ];
   };
