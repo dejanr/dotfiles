fd8688fcfa0a471f4f04d960cff68c016e0f7b0c
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2021-11-01 14:48:31 +0100

    revert audio

--- nix/config/nixpkgs/roles/multimedia.nix
+++ nix/config/nixpkgs/roles/multimedia.nix
@@ -21,7 +21,7 @@
     mplayer
     pamixer # cli tools for pulseaudio
     paprefs
-    pavucontrol
+    lxqt.pavucontrol-qt
     puredata
     qjackctl
     vlc
@@ -31,35 +31,31 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-      };
-    };
   };
 
   hardware.pulseaudio = {
-    enable = false;
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
-    package = pkgs.pulseaudioFull;
-    extraConfig = "
+    enable = true;
+    extraConfig = ''
+      load-module module-udev-detect tsched=0
+      load-module module-bluetooth-policy
+      load-module module-bluetooth-discover
       load-module module-switch-on-connect
-    ";
-  };
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
+      ### Enable Echo/Noise-Cancellation
+      load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args="analog_gain_control=0 digital_gain_control=1" source_name=echoCancel_source sink_name=echoCancel_sink
+      set-default-source echoCancel_source
+      set-default-sink echoCancel_sink
+    '';
+    extraModules = [
+      pkgs.pulseaudio-modules-bt
     ];
-    gtkUsePortal = true;
+
+    daemon.config = {
+      avoid-resampling = "yes";
+      default-sample-rate = 48000;
+    };
+
+    support32Bit = true;
   };
 
   services.minidlna = {
