634446193d95b918384a7c71603f272206b220be
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2022-02-01 17:40:36 +0100

    Update screenshot script

--- nix/config/nixpkgs/overlays/20-scripts/screenshot/script.nix
+++ nix/config/nixpkgs/overlays/20-scripts/screenshot/script.nix
@@ -1,6 +1,10 @@
 { pkgs }:
 
+let
+  hacksaw = "${pkgs.hacksaw}/bin/hacksaw";
+  shotgun = "${pkgs.shotgun}/bin/shotgun";
+in
 ''
-  selection=$(${pkgs.hacksaw} -f "-i %i -g %g")
-  ${pkgs.shotgun} $selection - | xclip -t 'image/png' -selection clipboard
+  selection=$(${hacksaw} -f "-i %i -g %g")
+  ${shotgun} $selection - | xclip -t 'image/png' -selection clipboard
 ''
