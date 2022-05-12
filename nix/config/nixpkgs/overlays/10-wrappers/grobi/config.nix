9e65c65a658be9bcb45b8b1844b10ea49dc8c97b
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2022-05-12 10:49:37 +0200

    Update grobi

--- nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
+++ nix/config/nixpkgs/overlays/10-wrappers/grobi/config.nix
@@ -18,7 +18,7 @@
         - HDMI-A-0
       atomic: true
       execute_after:
-        - xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 3440x1440 --pos 0x1080 --rotate normal --primary --output HDMI-A-0 --mode 1920x1080 --pos 760x0 --rotate normal --scale 0.5x0.5
+        - xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 3440x1440 --pos 0x1080 --rotate normal --output HDMI-A-0 --noprimary --mode 1920x1080 --pos 760x0 --rotate normal --scale 0.5x0.5
         - wm-wallpaper
 
     - name: Fallback
