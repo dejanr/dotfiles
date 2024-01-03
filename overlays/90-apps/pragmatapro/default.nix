187dec0fe5de6d1e1d0485a3a450a78dd98cc0b7
dejanr <dejan@ranisavljevic.com>
2024-01-03 13:11:27 +0000

    Update pragmatapro to latest

--- overlays/90-apps/pragmatapro/default.nix
+++ overlays/90-apps/pragmatapro/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, requireFile, unzip }:
 let
-  version = "0.828-2";
+  version = "1.0";
 in
 stdenv.mkDerivation rec {
   name = "pragmatapro-${version}";
   src = requireFile rec {
-    name = "PragmataPro${version}.zip";
+    name = "PragmataPro.zip";
     url = "file://path/to/${name}";
-    sha256 = "19q6d0dxgd9k2mhr31944wpprks1qbqs1h5f400dyl5qzis2dji3";
+    sha256 = "28c3170ae35e35f23514ba3ef109e8934d5cd1552984b79144cb42bb1527ca32";
     message = ''
       ${name} font not found in nix store, to add it run:
 
