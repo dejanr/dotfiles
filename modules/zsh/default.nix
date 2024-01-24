8ca26b39bc5b7afadefb67624fb87f9199a8ba47
dejanr <dejan@ranisavljevic.com>
2024-01-24 08:28:59 +0100

    Add zsh WORDCHARS and prefix search

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -2,132 +2,137 @@
 with lib;
 let cfg = config.modules.zsh;
 in {
-    options.modules.zsh = { enable = mkEnableOption "zsh"; };
+  options.modules.zsh = { enable = mkEnableOption "zsh"; };
 
-    config = mkIf cfg.enable {
-    	home.packages = [
-          pkgs.zsh
-        ];
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.zsh ];
 
-        programs.zsh = {
-            enable = true;
+    programs.zsh = {
+      enable = true;
 
-            # directory to put config files in
-            dotDir = ".config/zsh";
+      # directory to put config files in
+      dotDir = ".config/zsh";
 
-            enableCompletion = true;
-            enableAutosuggestions = true;
-            syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      syntaxHighlighting.enable = true;
 
-            # .zshrc
-            initExtra = ''
-                PROMPT="%F{white}%~%b "$'\n'"%(?.%F{white}λ%b.%F{red}λ) %f"
+      # .zshrc
+      initExtra = ''
+        PROMPT="%F{white}%~%b "$'\n'"%(?.%F{white}λ%b.%F{red}λ) %f"
 
-                if [ -f /opt/homebrew/bin/brew ]; then
-                    eval "$(/opt/homebrew/bin/brew shellenv)"
-                fi
+        if [ -f /opt/homebrew/bin/brew ]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"
+        fi
 
-                export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store";
-                export ZK_NOTEBOOK_DIR="~/stuff/notes";
-                export DIRENV_LOG_FORMAT="";
-                bindkey -e
-                bindkey '^ ' autosuggest-accept
-                bindkey '^R' history-incremental-search-backward
-                bindkey '^[[7~' beginning-of-line                               # Home key
-                bindkey '^[[H' beginning-of-line                                # Home key
-                if [[ "''${terminfo[khome]}" != "" ]]; then
-                    bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
-                fi
-                bindkey '^[[8~' end-of-line                                     # End key
-                bindkey '^[[F' end-of-line                                     # End key
-                if [[ "''${terminfo[kend]}" != "" ]]; then
-                    bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
-                fi
-                bindkey '^[[2~' overwrite-mode                                  # Insert key
-                bindkey '^[[3~' delete-char                                     # Delete key
-                bindkey '^[[C'  forward-char                                    # Right key
-                bindkey '^[[D'  backward-char                                   # Left key
-                bindkey '^[[5~' history-beginning-search-backward               # Page up key
-                bindkey '^[[6~' history-beginning-search-forward                # Page down key
-                # Navigate words with ctrl+arrow keys
-                bindkey '^[Oc' forward-word                                     #
-                bindkey '^[Od' backward-word                                    #
-                bindkey '^[[1;5D' backward-word                                 #
-                bindkey '^[[1;5C' forward-word                                  #
-                bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-                bindkey '^[[Z' undo                                             # Shift+tab undo last action
-                # Theming section
-                autoload -U colors
-                colors
+        export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store";
+        export ZK_NOTEBOOK_DIR="~/stuff/notes";
+        export DIRENV_LOG_FORMAT="";
+        bindkey -e
+        bindkey '^ ' autosuggest-accept
+        bindkey '^R' history-incremental-search-backward
+        bindkey '^[[7~' beginning-of-line                               # Home key
+        bindkey '^[[H' beginning-of-line                                # Home key
+        if [[ "''${terminfo[khome]}" != "" ]]; then
+            bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
+        fi
+        bindkey '^[[8~' end-of-line                                     # End key
+        bindkey '^[[F' end-of-line                                     # End key
+        if [[ "''${terminfo[kend]}" != "" ]]; then
+            bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
+        fi
+        bindkey '^[[2~' overwrite-mode                                  # Insert key
+        bindkey '^[[3~' delete-char                                     # Delete key
+        bindkey '^[[C'  forward-char                                    # Right key
+        bindkey '^[[D'  backward-char                                   # Left key
+        bindkey '^[[5~' history-beginning-search-backward               # Page up key
+        bindkey '^[[6~' history-beginning-search-forward                # Page down key
+        # Navigate words with ctrl+arrow keys
+        bindkey '^[Oc' forward-word                                     #
+        bindkey '^[Od' backward-word                                    #
+        bindkey '^[[1;5D' backward-word                                 #
+        bindkey '^[[1;5C' forward-word                                  #
+        bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
+        bindkey '^[[Z' undo                                             # Shift+tab undo last action
+        # Theming section
+        autoload -U colors
+        colors
 
-                edir() { tar -cz $1 | age -p > $1.tar.gz.age && rm -rf $1 &>/dev/null && echo "$1 encrypted" }
-                ddir() { age -d $1 | tar -xz && rm -rf $1 &>/dev/null && echo "$1 decrypted" }
+        edir() { tar -cz $1 | age -p > $1.tar.gz.age && rm -rf $1 &>/dev/null && echo "$1 encrypted" }
+        ddir() { age -d $1 | tar -xz && rm -rf $1 &>/dev/null && echo "$1 decrypted" }
 
-                ## case-insensitive (uppercase from lowercase) completion
-                zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+        ## case-insensitive (uppercase from lowercase) completion
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-                export PATH=node_modules/.bin:$PATH
-            '';
+        export PATH=node_modules/.bin:$PATH
+      '';
 
-            # basically aliases for directories:
-            # `cd ~dots` will cd into ~/.config/nixos
-            dirHashes = {
-                dots = "$HOME/.config/nixos";
-                stuff = "$HOME/stuff";
-                media = "/run/media/$USER";
-                junk = "$HOME/stuff/other";
-            };
+      localVariables = {
+        WORDCHARS = "*?_-.[]~=&;!#$%^(){}<>";
+        HISTORY_SUBSTRING_SEARCH_PREFIXED = "1";
+        HISTORY_IGNORE_ALL_DUPS = "1";
+      };
 
-            # Tweak settings for history
-            history = {
-                save = 1000;
-                size = 1000;
-                path = "$HOME/.cache/zsh_history";
-            };
+      # basically aliases for directories:
+      # `cd ~dots` will cd into ~/.config/nixos
+      dirHashes = {
+        dots = "$HOME/.config/nixos";
+        stuff = "$HOME/stuff";
+        media = "/run/media/$USER";
+        junk = "$HOME/stuff/other";
+      };
 
-            # Set some aliases
-            shellAliases = {
-                c = "clear";
-                mkdir = "mkdir -vp";
-                rm = "rm -rifv";
-                mv = "mv -iv";
-                cp = "cp -riv";
-                cat = "bat --paging=never --style=plain";
-                tree = "eza --tree";
-                nd = "nix develop -c $SHELL";
+      # Tweak settings for history
+      history = {
+        save = 1000;
+        size = 1000;
+        path = "$HOME/.cache/zsh_history";
+      };
 
-				e = "nvim -i NONE";
-				vi = "nvim -i NONE";
-				vim = "nvim -i NONE";
-				nvim = "nvim -i NONE";
+      # Set some aliases
+      shellAliases = {
+        c = "clear";
+        mkdir = "mkdir -vp";
+        rm = "rm -rifv";
+        mv = "mv -iv";
+        cp = "cp -riv";
+        cat = "bat --paging=never --style=plain";
+        tree = "eza --tree";
+        nd = "nix develop -c $SHELL";
 
-				l = "eza -a";
-				ll = "eza -la";
-                ls = "eza -a";
+        e = "nvim -i NONE";
+        vi = "nvim -i NONE";
+        vim = "nvim -i NONE";
+        nvim = "nvim -i NONE";
 
-				du = "du -hc";
+        l = "eza -a";
+        ll = "eza -la";
+        ls = "eza -a";
 
-				gd = "git diff";
-				gp = "git push";
-				gc = "git commit";
-				gca = "git commit -a";
-				gco = "git checkout";
-				gb = "git branch";
-				gs = "clear; git status -sb";
-				grm = "git status | grep deleted | awk '{print $3}' | xargs git rm";
+        du = "du -hc";
 
-				timestamp = "date +%s";
+        gd = "git diff";
+        gp = "git push";
+        gc = "git commit";
+        gca = "git commit -a";
+        gco = "git checkout";
+        gb = "git branch";
+        gs = "clear; git status -sb";
+        grm = "git status | grep deleted | awk '{print $3}' | xargs git rm";
 
-				passgen = "date +%s | shasum | base64 | head -c 8 | pbcopy | echo 'Password saved in clipboard'";
+        timestamp = "date +%s";
 
-				lmk = "notify-send 'Something happened!'";
+        passgen =
+          "date +%s | shasum | base64 | head -c 8 | pbcopy | echo 'Password saved in clipboard'";
 
-                run-last-history-in-vimux = "history | grep 'clear;' | grep -v 'grep clear;' | sort -n -r | head -n 1 | cut -d';' -f2- | xargs -i tmux send-keys -t 0 Escape :VimuxPromptCommand Enter 'clear; {}' Enter";
-            };
+        lmk = "notify-send 'Something happened!'";
 
-            # Source all plugins, nix-style
-            plugins = [
-        ];
+        run-last-history-in-vimux =
+          "history | grep 'clear;' | grep -v 'grep clear;' | sort -n -r | head -n 1 | cut -d';' -f2- | xargs -i tmux send-keys -t 0 Escape :VimuxPromptCommand Enter 'clear; {}' Enter";
+      };
+
+      # Source all plugins, nix-style
+      plugins = [ ];
     };
-};
+  };
 }
