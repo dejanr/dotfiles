96c2eca5f0d68a99546f7c601a7d13d58fa9c000
dejanr <dejan@ranisavljevic.com>
2025-01-01 16:47:47 +0100

    Add avante for LLM

--- modules/darwin/sketchybar/config/plugins/space.sh
+++ modules/darwin/sketchybar/config/plugins/space.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 update_space() {
+    echo "INFO: $INFO"
     SPACE_ID=$(echo "$INFO" | jq -r '."display-1"')
+    echo "SPACE_ID: $SPACE_ID"
 
     case $SPACE_ID in
     5)
@@ -16,11 +18,18 @@ update_space() {
         ;;
     esac
 
+    echo "ICON: $ICON"
+    echo "ICON_PADDING_LEFT: $ICON_PADDING_LEFT"
+    echo "ICON_PADDING_RIGHT: $ICON_PADDING_RIGHT"
+
     /opt/homebrew/opt/sketchybar/bin/sketchybar --set $NAME \
         icon=$ICON \
         icon.padding_left=$ICON_PADDING_LEFT \
         icon.padding_right=$ICON_PADDING_RIGHT
 }
+
+echo "SENDER: $SENDER"
+echo "NAME: $NAME"
 
 case "$SENDER" in
 *)
---
+++ modules/nvim2/config/lua/plugins/avante.lua
@@ -0,0 +1,50 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    provider = "groq",
+    vendors = {
+      groq = {
+        __inherited_from = "openai",
+        api_key_name = "GROQ_API_KEY",
+        endpoint = "https://api.groq.com/openai/v1/",
+        model = "llama-3.3-70b-versatile",
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "hrsh7th/nvim-cmp",
+    "echasnovski/mini.icons",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
