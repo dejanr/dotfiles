c54bbbcc8168cf336364496904ab8d6475551833
dejanr <dejan@ranisavljevic.com>
2025-01-05 22:09:00 +0100

    Dont notify lazy updates

--- modules/nvim/config/lua/config/lazy.lua
+++ modules/nvim/config/lua/config/lazy.lua
@@ -33,5 +33,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "nightfox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
