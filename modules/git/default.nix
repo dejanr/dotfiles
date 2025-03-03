cb088bfffbbaa063abe0b2ce68628fc73fbf1420
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-03-03 09:44:04 +0100

    Add different git configs for different projects

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -9,18 +9,39 @@ in {
     programs.git = {
       enable = true;
       lfs.enable = true;
-      userName = "dejanr";
+      userName = "Dejan Ranisavljevic";
       userEmail = "dejan@ranisavljevic.com";
       extraConfig = {
         init = { defaultBranch = "main"; };
         github = { user = "dejanr"; };
-  "filter \"lfs\"" = {
-     clean = "${pkgs.git-lfs}/bin/git-lfs clean -- %f";
-     smudge = "${pkgs.git-lfs}/bin/git-lfs smudge --skip -- %f";
-     process = "${pkgs.git-lfs}/bin/git-lfs filter-process --skip";
-     required = true;
-  };
+        "filter \"lfs\"" = {
+          clean = "${pkgs.git-lfs}/bin/git-lfs clean -- %f";
+          smudge = "${pkgs.git-lfs}/bin/git-lfs smudge --skip -- %f";
+          process = "${pkgs.git-lfs}/bin/git-lfs filter-process --skip";
+          required = true;
+        };
       };
+
+      includes = [
+        {
+          condition = "gitdir:~/projects/futurice/";
+          contents = {
+            user = {
+              email = "dejan.ranisavljevic@futurice.com";
+              name = "Dejan Ranisavljevic";
+            };
+          };
+        }
+        {
+          condition = "gitdir:~/projects/ororatech/";
+          contents = {
+            user = {
+              email = "dejan.ranisavljevic@ororatech.com";
+              name = "Dejan Ranisavljevic";
+            };
+          };
+        }
+      ];
     };
     programs.lazygit = {
       enable = true;
