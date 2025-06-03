1f2d43ce9c89baf7f0121d37c73a2c948a819db1
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-06-03 18:02:27 +0200

    lazy fix

--- modules/nvim/config/lua/config/lazy.lua
+++ modules/nvim/config/lua/config/lazy.lua
@@ -23,26 +23,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		-- add LazyVim and import its plugins
-		{ "LazyVim/LazyVim" },
-		-- import/override with your plugins
 		{ import = "plugins" },
 	},
 	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
 		lazy = false,
-		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-		-- have outdated releases, which may break your Neovim install.
 		version = false, -- always use the latest git commit
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "nightfox" } },
-	-- automatically check for plugin updates
 	checker = { enabled = true, notify = false },
 })
