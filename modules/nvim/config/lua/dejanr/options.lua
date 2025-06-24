eb455b09dfd07c879196f4cf494d590af99d45c3
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-06-24 15:28:26 +0200

    Add nix-cache

--- modules/nvim/config/lua/dejanr/options.lua
+++ modules/nvim/config/lua/dejanr/options.lua
@@ -1,4 +1,5 @@
 local o = vim.opt
+local aug = vim.api.nvim_create_augroup("dejanr", {})
 
 -- Leader
 
@@ -58,3 +59,16 @@ o.signcolumn = "yes:1"
 
 -- auto reload files changed outside of vim
 o.autoread = true
+
+vim.api.nvim_create_autocmd("FocusGained", {
+	desc = "Reload files from disk when we focus vim",
+	pattern = "*",
+	command = "if getcmdwintype() == '' | checktime | endif",
+	group = aug,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+	desc = "Every time we enter an unmodified buffer, check if it changed on disk",
+	pattern = "*",
+	command = "if &buftype == '' && !&modified && expand('%') != '' | exec 'checktime ' . expand('<abuf>') | endif",
+	group = aug,
+})
--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -52,7 +52,6 @@ in
     };
   };
 
-  # Nix settings, auto cleanup and enable flakes
   nix = {
     settings = {
       auto-optimise-store = true;
@@ -61,15 +60,12 @@ in
 
       substituters = [
         "ssh://nix-cache"
-        "ssh://eu.nixbuild.net"
         "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.lix.systems"
       ];
       trusted-public-keys = [
         "ot-nix-cache:C6ZY7QNJHk8tAcyi00y0n3UhbnZvBxJE993/J61omU4="
-        "nixbuild.net/ororatech-swuerl-1:pIlkdwXcQ4rhAhyI17SLno25zgfeWFbBPBnA0jvIXyM="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
