bafbfa7d4c03b143e69c59881be42779226ce4cb
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-07-20 11:46:59 +0200

    Remove substrituters

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -62,17 +62,6 @@ in
         "${username}"
         "root"
       ];
-
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://cache.lix.systems"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      ];
     };
 
     gc = {
