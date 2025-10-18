396f78b63141c366dc98da5f078f56afaf00d07e
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2025-10-18 13:28:35 +0200

    Update i3

--- overlays/10-wrappers/i3-config/config.nix
+++ overlays/10-wrappers/i3-config/config.nix
@@ -285,10 +285,6 @@
   for_window [class="Albion-Online"] floating enable
   for_window [class="Albion-Online"] resize set 3440 1440
   for_window [class="Albion-Online"] move window to position 0 -20
-  for_window [class="steam_app_1063730"] fullscreen disable
-  for_window [class="steam_app_1063730"] floating enable
-  for_window [class="steam_app_1063730"] resize set 3840 2140
-  for_window [class="steam_app_1063730"] move window to position 0 20
   for_window [class="steam_app_327070"] fullscreen disable
   for_window [class="steam_app_327070"] floating enable
   for_window [class="steam_app_327070"] resize set 3440 1400
