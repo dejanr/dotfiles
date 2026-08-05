6d79e6d7bfdb7773d0e8fe89cbd352c2a789e491
Dejan Ranisavljevic <dejan@ranisavljevic.com>
2026-08-05 10:22:07 +0200

    Add new mbp-work ssh key for agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,13 @@
 let
   agenixKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPvj/ZE/vb6CWIhmcHgYtm0qryvK/3whB8RZ68tzqFk";
+  burdaKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpUACVodxHSbI3hAFm8t4YypQPoAuEyPzQA6JUIFZEr dejan.ranisavljevic@burda-forward.de";
   frameworkHostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1kEkLAbGZb9u9IiXpV/q92dRnmW3xm8Ysn0QdHp++D root@framework";
 
-  userKeys = [ agenixKey ];
-  caddyKeys = [
+  userKeys = [
     agenixKey
-    frameworkHostKey
+    burdaKey
   ];
+  caddyKeys = userKeys ++ [ frameworkHostKey ];
 in
 {
   "anthropic_api_key.age".publicKeys = userKeys;
